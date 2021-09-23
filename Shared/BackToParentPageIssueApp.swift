73c6599d76aaeef5d6a1e5a4f0aa54681038901a
kavisha <kavisha_jg@yahoo.com>
2021-09-23 10:44:38 +0530

    Initial Commit

---
+++ Shared/BackToParentPageIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackToParentPageIssueApp.swift
+//  Shared
+//
+//  Created by Kavisha Sonaal on 23/09/21.
+//
+
+import SwiftUI
+
+@main
+struct BackToParentPageIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
